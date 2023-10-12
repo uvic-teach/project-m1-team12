0f0766c816df6cf061ef66293f226515ab38ab8e
Scott <skenning128@gmail.com>
2023-10-12 13:47:14 -0700

    link to microservices

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="w-screen h-screen p-4 md:px-24 md:py-12 bg-orange-100">
       <div className="grid grid-cols-2 gap-4 md:gap-8 h-full w-full">
         <HomeButton to='/menu' title='Menu' />
-        <HomeButton to='/emergency' title='Emergency' />
+        <HomeButton to='/emergency' title='Emergency (currently links to login microservice)' />
         <HomeButton to='/calendar' title='Calendar' />
         <HomeButton to='/messaging' title='Messaging' />
       </div>
@@ -24,7 +24,7 @@ export default function Home() {
 const HomeButton = ({to, title}: HomeButtonProps) => {
   return (
     <Link href={to} underline="none" className='h-full'>
-      <Card className={`w-full h-full flex grid items-center ${title === "Emergency" && "bg-red-200 border-red-400 border-2 text-red-900"}`}>
+      <Card className={`w-full h-full p-8 flex grid items-center ${title === "Emergency" && "bg-red-200 border-red-400 border-2 text-red-900"}`}>
         <Typography className="text-center text-2xl">{title}</Typography>
       </Card>
     </Link>
