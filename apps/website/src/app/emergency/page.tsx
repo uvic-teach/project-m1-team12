562b5001934edfd834a07739cb0d341c19a27958
Scott <skenning128@gmail.com>
2023-10-15 14:42:11 -0700

    fixing link on main

--- apps/website/src/app/emergency/page.tsx
+++ apps/website/src/app/emergency/page.tsx
@@ -3,7 +3,7 @@ import { ExampleFetch } from "../components/ExampleFetch";
 export default function Emergency() {
     return (
         <div className="w-screen min-h-screen text-xl flex flex-col items-center justify-center gap-8">
-            <ExampleFetch url="https://login-microservice.fly.dev" />
+            <ExampleFetch url="https://resident-management.fly.dev" />
         </div>
     )
 }
