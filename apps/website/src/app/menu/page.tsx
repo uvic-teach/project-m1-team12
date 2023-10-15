9eed5cf193c5cee2d4fdd69ca4d7d62a7de37159
Scott <skenning128@gmail.com>
2023-10-15 14:37:02 -0700

    update link

--- apps/website/src/app/menu/page.tsx
+++ apps/website/src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import { ExampleFetch } from "../components/ExampleFetch";
 export default function Menu() {
     return (
         <div className="w-screen min-h-screen text-xl flex flex-col items-center justify-center gap-8">
-            <ExampleFetch url="https://events-microservice.fly.dev" />
+            <ExampleFetch url="https://resident-management.fly.dev" />
         </div>
     )
 }
