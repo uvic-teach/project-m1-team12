367a7b152d2822d3dfbf60a33d889f83a12bb60b
Jayna <bettesworthjayna@gmail.com>
2023-10-29 14:42:53 -0700

    configure register and login pages

---
+++ apps/website/src/app/login/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import React, { useState } from 'react' 
+
+
+export default function Registration() {
+    const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+
+    async function login(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault()
+
+		const response = await fetch('http://localhost:8082/user/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+			console.log(data)
+
+	}
+
+    return (
+        <form className="flex flex-col gap-2 mx-auto max-w-md mt-10 " onSubmit={login}>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="Email"
+				/>
+				<br />
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					placeholder="Password"
+				/>
+				<br />
+				<input type="submit" value="Register" />
+        </form>
+    )
+}
---
+++ apps/website/src/app/registration/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React, { useState } from 'react' 
+
+
+export default function Registration() {
+    const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+
+    async function registerUser(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault()
+
+		const response = await fetch('http://localhost:8082/user/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name,
+				email,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+
+		if (data.status === 'ok') {
+			console.log(data)
+		}
+	}
+
+    return (
+        <form className="flex flex-col gap-2 mx-auto max-w-md mt-10 " onSubmit={registerUser}>
+           <input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					type="text"
+					placeholder="Name"
+				/>
+				<br />
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="Email"
+				/>
+				<br />
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					placeholder="Password"
+				/>
+				<br />
+				<input type="submit" value="Register" />
+        </form>
+    )
+}
