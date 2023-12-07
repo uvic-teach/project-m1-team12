3f9bd088919ef4c921d5f41b2a574e121e44f2bb
SteapMunk <github_slea@gsi.bc.ca>
2023-12-07 12:34:47 -0800

    Changed button from emergency to members

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -26,10 +26,10 @@ export default function Home() {
   return (
     <main className="w-screen h-screen p-8 md:px-24 md:py-12 bg-gray-900 text-white">
       <div className="grid md:grid-cols-2 gap-4 md:gap-8 h-full w-full">
-        <HomeButton to='/menu' title='Menu' />
-        <HomeButton to='/emergency' title='Emergency' />
-        <HomeButton to='/calendar' title='Calendar' />
-        <HomeButton to='/messaging' title='Messaging' />
+        <HomeButton to='/menu' title='menu'/>
+        <HomeButton to='/members' title='Members'/>
+        <HomeButton to='/calendar' title='Calendar'/>
+        <HomeButton to='/messaging' title='Messaging'/>
       </div>
     </main>
   )
@@ -39,10 +39,10 @@ const HomeButton = ({to, title}: HomeButtonProps) => {
   // I removed the material UI components because it was slowing the load time to > 800ms
   // Feel free to add back if you can figre out why it took so long to load
   return (
-    <Link href={to} className='h-full'>
-      <div className="rounded-lg w-full h-full p-8 flex grid items-center border-2 border-gray-700 bg-gray-800 hover:scale-[101%] hover:bg-gray-800/90">
-        <p className="text-center text-3xl text-white">{title}</p>
-      </div>
-    </Link>
+      <Link href={to} className='h-full'>
+        <div className="rounded-lg w-full h-full p-8 flex grid items-center border-2 border-gray-700 bg-gray-800 hover:scale-[101%] hover:bg-gray-800/90">
+          <p className="text-center text-3xl text-white">{title}</p>
+        </div>
+      </Link>
   )
 }
