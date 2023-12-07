daca1a7468112a9ad36b491ecfc3e868bccdfd0e
SteapMunk <github_slea@gsi.bc.ca>
2023-12-07 12:33:18 -0800

    Added Home Button and add user button to members page

--- apps/website/src/app/members/page.tsx
+++ apps/website/src/app/members/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, {ButtonHTMLAttributes, useEffect, useState} from 'react'
+import Link from "next/link";
 
 
 export default function Members() {
@@ -27,7 +28,6 @@ export default function Members() {
         }
         fetchData();
     }, [])
-
 
 
     async function checkAdmin(){
@@ -82,34 +82,38 @@ export default function Members() {
                 <td className="whitespace-nowrap px-6 py-4 font-medium">{name}</td>
                 <td className="whitespace-nowrap px-6 py-4 font-medium">{email}</td>
                 <td className="justify-end">
-                    <button className="bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    <button className="bg-red-900 hover:bg-red-950 text-white font-bold py-2 px-4 rounded"
                             onClick={() => deleteUser(name)}>
                         delete {name}
                     </button>
                 </td>
-
             </tr>
     )
 
-
     return (
         <main className='bg-gray-900 text-gray-300 w-screen min-h-screen flex flex-col items-center gap-4 py-8'>
-            <h2 className='text-3xl'>Members</h2>
-            <div className="flex flex-col overflow-x-auto sm:-mx-6 lg:-mx-8 inline-block min-w-full py-2 sm:px-6 lg:px-8 overflow-hidden">
+            <div>
+                <h2 className='text-3xl'>Members</h2>
+            </div>
+            <div>
+                <Link href='/' className="bg-indigo-800 p-2 mx-1 rounded-3xl">Home</Link>
+                <Link href='/registration' className="bg-indigo-800 p-2 mx-1 rounded-3xl">Add User</Link>
+            </div>
+            <div
+                className="flex flex-col overflow-x-auto sm:-mx-6 lg:-mx-8 inline-block min-w-full py-2 sm:px-6 lg:px-8 overflow-hidden">
                 <table className=' min-w-full text-left text-sm font-light'>
                     <thead className="border-b font-medium dark:border-neutral-300">
-                        <tr className="">
-                            <th className="px-6 py-4">Name</th>
-                            <th className="px-6 py-4">Email</th>
-                            <th className="px-6 py-4">Delete User</th>
-                        </tr>
+                    <tr className="">
+                        <th className="px-6 py-4">Name</th>
+                        <th className="px-6 py-4">Email</th>
+                        <th className="px-6 py-4">Delete User</th>
+                    </tr>
                     </thead>
                     <tbody className="justify-center">
                     {userData.map(userRow)}
                     </tbody>
                 </table>
             </div>
-
         </main>
     )
 }
